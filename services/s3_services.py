c8d9e13f6bdd6a07b3d118dfdda0d875f82f66b4
Álefe Cardozo Silva <alefe@192.168.0.10>
2025-05-04 15:15:36 -0300

    ajuste no codigo para rodar CI

--- services/s3_services.py
+++ services/s3_services.py
@@ -1,8 +1,12 @@
 import boto3
+import logging
 
+log = logging.getLogger(__name__)
 
 class S3Services:
+    @staticmethod
     def criar_client(aws_access_key, aws_secret_key, region):
+        log.info("##Criando conexao com a AWS e obtendo credenciais##")
         return boto3.client(
             's3',
             aws_access_key_id=aws_access_key,
@@ -10,6 +14,9 @@ class S3Services:
             region_name=region
         )
 
+    @staticmethod
     def enviar_arquivo_s3(s3_client, bucket, arquivo_local, chave_s3):
+        log.info(f"Enviando arquivo '{arquivo_local}' para bucket '{bucket}' na chave '{chave_s3}'")
         s3_client.upload_file(arquivo_local, bucket, chave_s3)
+        log.info("Upload realizado com sucesso.")
         return True
--- steps/s3_steps.py
+++ steps/s3_steps.py
@@ -1,27 +1,27 @@
 import os
 from behave import given, when, then
-
 from services.s3_services import S3Services
+import logging
+logging.basicConfig(level=logging.INFO)
 
+log = logging.getLogger(__name__)
 
-@given('que eu possuo um arquivo "{arquivo}"')
+@given('que eu possuo um arquivo "{arquivo} e as credenciais da AWS válidas')
 def step_impl(context, arquivo):
     os.makedirs('data', exist_ok=True)
     context.arquivo = f'data/{arquivo}'
-    # Cria o arquivo para garantir que ele existe
-    with open(context.arquivo, 'w') as f:
-        f.write('{"nome": "StackSpot"}')
 
-
-@given('as credenciais da AWS válidas')
-def step_impl(context):
     context.aws_access_key = os.getenv('AWS_ACCESS_KEY_ID')
     context.aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-    context.region = os.getenv('AWS_REGION', 'sa-east-1')
-
+    context.region = 'sa-east-1'
+    # log.info("## given method ##")
+    # log.info(f"AWS_ACCESS_KEY_ID: {context.aws_access_key}")
+    # log.info(f"AWS_SECRET_ACCESS_KEY: {'*' * len(context.aws_secret_key) if context.aws_secret_key else ''}")
+    # log.info(f"AWS_REGION: {context.region}")
 
 @when('eu envio o arquivo para o bucket "{bucket}" na chave "{chave}"')
 def step_impl(context, bucket, chave):
+    log.info(f"Preparando para enviar arquivo '{context.arquivo}' para bucket '{bucket}' na chave '{chave}'")
     context.bucket = bucket
     context.chave = chave
     context.s3_client = S3Services.criar_client(
@@ -30,10 +30,12 @@ def step_impl(context, bucket, chave):
     context.resultado = S3Services.enviar_arquivo_s3(
         context.s3_client, bucket, context.arquivo, chave
     )
-
+    log.info("Arquivo enviado com sucesso.")
 
 @then('o arquivo deve estar disponível no bucket')
 def step_impl(context):
+    log.info(f"Verificando se o arquivo '{context.chave}' está disponível no bucket '{context.bucket}'")
     response = context.s3_client.list_objects_v2(Bucket=context.bucket, Prefix=context.chave)
     assert 'Contents' in response
     assert any(obj['Key'] == context.chave for obj in response['Contents'])
+    log.info("Arquivo encontrado no bucket.")
